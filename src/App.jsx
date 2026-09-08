fc6babdd18b100e03375caa9df5dfab363cf383f
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-08 18:15:01 +0600

    Handling side effect and cleanup function

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [anotherCount, setAnotherCount] = useState(0);
+  const [second, setSecond] = useState(0);
   
   useEffect(()=> {
-    console.log("Use Effect Run");
-  }, [count]);
+    const timerId = setInterval(()=>{
+      setSecond((prev)=> prev + 1)
+    }, 1000);
+    return ()=> clearInterval(timerId);
+  }, []);
   return (
     <>
-      <button onClick={()=> setCount(count+1)}>
-        Increment ({count})
-      </button>
-      <button onClick={()=> setAnotherCount(anotherCount+1)}>
-        Increment Another ({anotherCount})
-      </button>
+      <h1>{second}</h1>
     </>
   )
 }
