e0edaec8e490fd187cc4bc150fe5b3bb711f4148
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-08 10:30:57 +0600

    User input recap and state update

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,28 @@ import './App.css'
 import Card from './components/Card'
 // import Counter from './components/Counter';
 
-const members = [
-  {name : "Shakib", age: 10},
-  {name : "Rakib", age: 60},
-  {name : "Rahim", age: 42},
-  {name : "Karim", age: 26}
-];
 
 function App() {
     const [name, setName] = useState("");
-
+    const [age, setAge] = useState("");
+    const [members, setMembers] = useState([
+      {name : "Shakib", age: 10},
+      {name : "Rakib", age: 60},
+      {name : "Rahim", age: 42},
+      {name : "Karim", age: 26}
+    ]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name)
-
+        setMembers({name, age})
+        console.log(members);
     };
 
   return (
     <>
     <form onSubmit={handleSubmit}>
         <input type="text" onChange={(e)=>setName(e.target.value)} />
+        <input type="number" onChange={(e)=>setAge(e.target.value)} />
         <button type='submit'>Submit</button>
     </form>
      {members.map((member) => (
