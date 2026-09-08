b7bca63a9eb14b69eff85790ac13cc279f5884cc
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-08 07:50:49 +0600

    Getting Started with react hooks - useState

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,56 +1,25 @@
+import { useState } from "react";
 import "./Card.css"
-// const Card = ({name, age}) => {
-
-//     if(age < 18) {
-//         return <div className="card">"{name} You are too young"</div>
-//     }
-
-//     return (
-//         <div className="card">
-//             <p>Name: {name}</p>
-//             <p>Age: {age}</p>
-//         </div>
-//     )
-// }
-
-// export default Card
-
-
-
-// const Card = ({name, age}) => {
-
-//     return (
-//         <>
-//           {age > 18 && <div className="card">
-//             <p>Name: {name}</p>
-//             <p>Age: {age}</p>
-//           </div>}
-
-//           {age < 18 && <div className="card">
-//             {name} You are too young
-//           </div>}
-//         </>
-//     )
-// }
-
-// export default Card
-
-
 
 
 const Card = ({name, age}) => {
 
+const [member, setMember] = useState("");
+
+  const clickHandler = (memberName) => {
+    console.log("Greeting " + memberName);
+  };
+
+  console.log("Member:", member);
+
     return (
-        <>
-          {age > 18 ? (
-            <div className="card">
-            <p>Name: {name}</p>
-            <p>Age: {age}</p>
-          </div>) : <div className="card">
-            {name} You are too young
-          </div>}
-        </>
-    )
+        <div  
+          onClick={() => clickHandler(name)}
+          className="card">
+          <p>Name: {name}</p>
+          <p>Age: {age}</p>
+        </div>
+    );
 }
 
 export default Card
